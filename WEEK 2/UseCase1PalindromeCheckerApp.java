4cbbb6cc34393a9d5d0c851de811ae45e06d753b
madhan175 <dmadhan692@gmail.com>
2026-02-18 15:04:53 +0530

    one application done

---
+++ WEEK 2/UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,18 @@
+public class UseCase1PalindromeCheckerApp {
+  static final app_name = "Palindrome Checker Management System";
+  static final app_vesrion = "vesrion:1";
+
+    public static void main (string () args[]{
+     dispalyqwelcomemessge9();
+
+
+    }
+
+  public static void main() {
+
+        System.out.println("         Welcome to the " + APP_NAME     );
+        System.out.println("          Version : " + APP_VERSION      );
+        System.out.println("    System initialized successfully     ");
+
+    }
+}
